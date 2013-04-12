2666f70b07d57327692f535dfa1499fa94b9032b
Chris Berkhout <chrisberkhout@gmail.com>
2013-04-12 10:18:49 +1000

    Removed upx (Ultimate Packer for eXecutables - http://upx.sourceforge.net/), because it only reduced the file from ~8MB to ~6MB. It was a complication that wasn't pulling its weight.

--- app/create_exe2.bat
+++ app/create_exe2.bat
@@ -4,8 +4,6 @@ python create_exe2.py py2exe
 xcopy /s /y dist\PoziConnect.exe ..
 REM xcopy /s /y ..\bb.ini ..\PoziConnect.ini
 
-include\upx.exe ..\PoziConnect.exe
-
 ECHO.
 ECHO Process completed. 
 
