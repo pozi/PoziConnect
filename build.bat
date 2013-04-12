89c513172e9b6331bf9c8297a3af59f611413f3f
Chris Berkhout <chrisberkhout@gmail.com>
2013-04-12 15:34:41 +1000

    Fixed build script to prepare a fully working release under the dist\ directory.

--- build.bat
+++ build.bat
@@ -1,9 +1,22 @@
 call setenv_python.bat
+
+rmdir /S /Q build
+rmdir /S /Q dist
+
 python app\build.py py2exe
 
-xcopy /s /y dist\PoziConnect.exe .
+rmdir /S /Q build
+del dist\w9xpopen.exe
 
-ECHO Process completed. 
+copy "%PYTHON_DIR%\m*s*90*" dist
 
-SET /P cname=[Press ENTER to close window]
+mkdir dist\vendor
+for /f "delims=" %%a in ('dir vendor\*gdal* /B') do @set GDAL_NAME=%%a
+xcopy /E "vendor\%GDAL_NAME%" dist\vendor\%GDAL_NAME%\
+
+copy PoziConnect.ini dist\
+xcopy /E tasks dist\tasks\
+xcopy /E recipes dist\recipes\
+mkdir dist\input
+mkdir dist\output
 
